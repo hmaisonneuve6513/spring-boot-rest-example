439896610c08e7a99b7eb3ae226c7145601d4db0
herve <hmaisonneuve6513@gmail.com>
2019-04-18 12:01:04 +0200

    add actuator micrometer exposer for prometheus

--- src/main/java/com/khoubyari/example/Application.java
+++ src/main/java/com/khoubyari/example/Application.java
@@ -27,9 +27,6 @@ public class Application extends SpringBootServletInitializer {
     private static final Class<Application> applicationClass = Application.class;
     private static final Logger log = LoggerFactory.getLogger(applicationClass);
 
-    @Autowired
-    private MeterRegistry registry;
-
     public static void main(String[] args) {
 		SpringApplication.run(applicationClass, args);
 	}
